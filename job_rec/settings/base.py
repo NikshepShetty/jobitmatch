4cda28b3bae1f4f8d0f243e498974a2b484534fa
captainphil21 <21niksshetty@gmail.com>
2021-03-18 21:48:26 +0530

    v0.0.2

--- job_rec/settings/base.py
+++ job_rec/settings/base.py
@@ -175,9 +175,10 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_SIGNUP_FORM_CLASS = 'pages.forms.SignupForm'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'NVsquare.business@gmail.com'
-EMAIL_HOST_PASSWORD = 'NVsquareadmin'
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
