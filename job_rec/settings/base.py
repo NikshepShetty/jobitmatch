3f6613e1d6f0f4bfe48639c2ce3caa7053e38e09
captainphil21 <21niksshetty@gmail.com>
2021-03-18 17:02:01 +0530

    v0.0.2

--- job_rec/settings/base.py
+++ job_rec/settings/base.py
@@ -161,7 +161,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 
 SITE_ID = 1
 
--- job_rec/settings/prod.py
+++ job_rec/settings/prod.py
@@ -5,6 +5,5 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost",'https://jobitmatch.herokuapp.com/']
+ALLOWED_HOSTS = ["127.0.0.1","localhost",'jobitmatch.herokuapp.com']
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
