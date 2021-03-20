a1680207a9f845ce470c6fad447ce29808b8b61b
captainphil21 <21niksshetty@gmail.com>
2021-03-20 16:27:37 +0530

    v0.0.2

--- job_rec/settings/prod.py
+++ job_rec/settings/prod.py
@@ -5,5 +5,5 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1","localhost",'jobitmatch.herokuapp.com','jobitmatch.com']
+ALLOWED_HOSTS = ["127.0.0.1","localhost",'jobitmatch.herokuapp.com','jobitmatch.com','www.jobitmatch.com']
 
