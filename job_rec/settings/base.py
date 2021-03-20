d837f9e2ae9a0e70fba0f0a48b559a8a82256fd1
captainphil21 <21niksshetty@gmail.com>
2021-03-20 14:53:22 +0530

    v0.0.2

--- job_rec/settings/base.py
+++ job_rec/settings/base.py
@@ -149,13 +149,12 @@ MEDIA_URL = '/media/'
 
 CACHES = {
     "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    },
+         "BACKEND": "redis_cache.RedisCache",
+         "LOCATION": os.environ.get('REDIS_URL'),
+    }
 }
+
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
